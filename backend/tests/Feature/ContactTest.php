f5831a36fec891d024a545b743f0105f8c5d7421
Arzaqul Mughny <zaarza03@gmail.com>
2023-10-05 12:37:58 +0700

    features(contact): add contact test

--- backend/tests/Feature/ContactTest.php
+++ backend/tests/Feature/ContactTest.php
@@ -37,4 +37,20 @@ class ContactTest extends TestCase
             'accept' => 'application/json'
         ])->assertStatus(401);
     }
+
+    public function testAddContact() {
+        DB::beginTransaction();
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        // make sure user1 & user2 contact is empty
+        $this->assertEmpty(User::where('id', $user1->id)->first()->contacts);
+        $this->assertEmpty(User::where('id', $user2->id)->first()->contacts);
+
+        Sanctum::actingAs($user1);
+        $this->post('/api/contacts/'. $user2->id, [], ['accept' => 'application/json'])->assertStatus(201);
+
+        $this->assertNotEmpty($user1->contacts);
+        $this->assertNotEmpty($user2->contacts);
+    }
 }
