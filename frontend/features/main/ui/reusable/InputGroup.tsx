38e6454efa49bf84fd2dcf80197bc283fbf50e41
zaarza <zaarza03@gmail.com>
2023-10-15 18:54:54 +0700

    refactor(input group)

--- frontend/features/main/ui/reusable/InputGroup.tsx
+++ frontend/features/main/ui/reusable/InputGroup.tsx
@@ -6,13 +6,7 @@ type InputGroupProps = {
     label?: string;
 };
 
-const InputGroup = ({
-    type,
-    name,
-    placeholder,
-    formikObject,
-    label,
-}: InputGroupProps) => {
+const InputGroup = ({ type, name, placeholder, formikObject, label }: InputGroupProps) => {
     return (
         <div className='form-group flex flex-col gap-y-3'>
             <label
@@ -23,23 +17,17 @@ const InputGroup = ({
             </label>
             <input
                 className={`bg-background focus:outline-primary rounded-lg py-4 px-8 border ${
-                    formikObject.status?.errors[name]
-                        ? 'border-red-500'
-                        : 'border-black/10'
+                    formikObject.errors[name] ? 'border-red-500' : 'border-black/10'
                 }`}
                 type={type}
                 name={name}
                 id={name}
-                placeholder={
-                    placeholder || name[0].toUpperCase() + name.slice(1)
-                }
+                placeholder={placeholder || name[0].toUpperCase() + name.slice(1)}
                 {...formikObject.getFieldProps(name)}
                 disabled={formikObject.isSubmitting}
             />
-            {formikObject.status?.errors[name] && (
-                <small className='self-end text-red-500 text-base'>
-                    formikObject.status.errors[name]
-                </small>
+            {formikObject.errors[name] && (
+                <small className='self-end text-red-500 text-base'>{formikObject.errors[name]}</small>
             )}
         </div>
     );
